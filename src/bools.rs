84d9bbdb386579d3025a348daf0b917f12474a98
chmodx <chmod777.ke@gmail.com>
2020-12-05 11:11:39 +0300

    chore: bools

---
+++ src/bools.rs
@@ -0,0 +1,12 @@
+pub fn run(){
+    // boolean types are represented as bool
+    // Has 2 values => true and false
+    // Are 1 byte (8 bits) in size
+
+    let _t = true;
+    let _f: bool = false;
+
+    let is_greater: bool = 10 > 5;
+
+    println!("10 is greater than 5 evaluates to {}",is_greater);
+}
