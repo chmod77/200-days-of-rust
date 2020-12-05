2f2cb5aa2120b9fc431ffe89d1164f5c2a68098e
chmodx <chmod777.ke@gmail.com>
2020-12-05 11:11:49 +0300

    chore: add chars

---
+++ src/chars.rs
@@ -0,0 +1,10 @@
+pub fn run(){
+    // Character type
+    // Are 4 bytes in size
+    // represented by single quotes
+    // 
+    let cee = 'c';
+    let zee = 'z';
+    let heart_eyed_cat = '😻';
+    println!("The chars are {}, {} and {}", cee, zee, heart_eyed_cat);
+}
