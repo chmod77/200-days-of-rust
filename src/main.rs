a9a8d915a71ce14acaa607f48b3706ed203a446d
chmodx <chmod777.ke@gmail.com>
2020-12-05 16:16:46 +0300

    chore: register mods

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 // mod scalars;
 mod complexes;
+mod functions;
 // use crate::scalars::bools;
 // use crate::scalars::chars;
 // use crate::scalars::strs;
 // use crate::scalars::scalar;
 
-use crate::complexes::tuple;
-
+// use crate::complexes::tuple;
+use crate::complexes::array;
+use crate::functions::params;
 fn main() {
     // chars::run();
     // bools::run();
-    tuple::run();
+    // tuple::run();
+    array::run();
+    params::run(12);
 }
