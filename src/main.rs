671699da093512c17ccc4278a4617de135d64d62
chmodx <chmod777.ke@gmail.com>
2020-12-26 15:09:17 +0300

    chore: include vectors to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // mod scalars;
 mod complexes;
 mod functions;
+
 // use crate::scalars::bools;
 // use crate::scalars::chars;
 // use crate::scalars::strs;
@@ -8,11 +9,13 @@ mod functions;
 
 // use crate::complexes::tuple;
 use crate::complexes::array;
+use crate::complexes::vectors;
 use crate::functions::params;
 fn main() {
     // chars::run();
     // bools::run();
     // tuple::run();
     array::run();
+    vectors::run();
     params::run(12);
 }
