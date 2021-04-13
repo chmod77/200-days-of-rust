1ae006c150d0df28846df8459e7c6e2942c2a6ac
chmodx <chmod777.ke@gmail.com>
2021-04-13 05:53:52 +0300

    chore: update main

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,28 @@
 mod complexes;
 mod functions;
 mod prints;
+mod scalars;
+mod names;
+
+
 // use crate::scalars::bools;
 // use crate::scalars::chars;
-// use crate::scalars::strs;
+use crate::scalars::strs;
+use crate::names::display_names;
 // use crate::scalars::scalar;
 
 // use crate::complexes::tuple;
-use crate::complexes::array;
-use crate::functions::params;
+// use crate::complexes::array;
+// use crate::functions::params;
 use crate::prints::display;
 
 fn main() {
     // chars::run();
     // bools::run();
     // tuple::run();
-    array::run();
-    params::run(12);
+    strs::run("Shalom".to_string());
+    display_names::run("Dwayne".to_string());
+    // array::run();
+    // params::run(12);
     display::run();
 }
